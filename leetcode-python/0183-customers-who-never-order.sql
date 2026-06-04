402a976c0f0ca95890902f1570850ae4b8fb81b7
Konrad Zatorski <kodiz2005@gmail.com>
2026-06-04 21:18:36 +0200

    Solve 183 LeetCode problem

---
+++ leetcode-python/0183-customers-who-never-order.sql
@@ -0,0 +1,6 @@
+-- Write your MySQL query statement below
+SELECT Customers.name AS "Customers" FROM Customers
+WHERE Customers.id NOT IN
+(SELECT Customers.id FROM Customers
+INNER JOIN Orders ON Orders.customerId = Customers.id
+GROUP BY Customers.name)
