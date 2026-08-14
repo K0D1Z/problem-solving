fc773d87d589254280688432dda4e2ac4d7b83cd
Konrad Zatorski <kodiz2005@gmail.com>
2026-08-14 14:48:25 +0200

    Time: 9 ms (81.13%), Space: 19.5 MB (46.22%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        # HASHMAP SOLTUTION
+        if len(s) != len(t):
+            return False
+
+        hashmap_1 = {}
+        hashmap_2 = {}
+
+        for i in range(len(s)):
+            hashmap_1[s[i]] = 1 + hashmap_1.get(s[i], 0)
+            hashmap_2[t[i]] = 1 + hashmap_2.get(t[i], 0)
+        
+        for c in hashmap_1:
+            if hashmap_1.get(c, 0) != hashmap_2.get(c, 0):
+                return False
+
+        return True
