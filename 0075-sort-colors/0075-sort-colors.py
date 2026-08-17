9645f3c31e58147966e191524f40d80075b99118
Konrad Zatorski <kodiz2005@gmail.com>
2026-08-17 21:13:43 +0200

    Time: 0 ms (100%), Space: 19.3 MB (61.96%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -4,14 +4,24 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         # STRAIGHTFORWARD SOLUTION
-        arr = [0,0,0]
-        for i in nums:
-            arr[i] += 1
-        j = 0
-        for k in range(len(arr)):
-            while arr[k] > 0:
-                nums[j] = k
-                arr[k] -= 1
-                j += 1
+        # arr = [0,0,0]
+        # for i in nums:
+        #     arr[i] += 1
+        # j = 0
+        # for k in range(len(arr)):
+        #     while arr[k] > 0:
+        #         nums[j] = k
+        #         arr[k] -= 1
+        #         j += 1
         
-        
+        # ONE PASS PARTITION SOLUTION
+        slow_ptr = 0
+        fast_ptr = 0
+        for val in range(3):            
+            for _ in range(len(nums) - fast_ptr):
+                if nums[fast_ptr] == val:
+                    nums[slow_ptr], nums[fast_ptr] = nums[fast_ptr], nums[slow_ptr]
+                    slow_ptr += 1
+                fast_ptr += 1
+            fast_ptr = slow_ptr
+
