1c787b6ce9a5e0aa973bbd7d4180f445b3b22c90
Konrad Zatorski <kodiz2005@gmail.com>
2026-09-04 18:17:41 +0200

    Time: 122 ms (22.95%), Space: 35.1 MB (25.54%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -4,21 +4,47 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
 
-        i = 0
-        reps = 0
-        temp = nums[0]
-        l = len(nums)
+        # FIRST APPROACH
+        
+        # i = 0
+        # reps = 0
+        # temp = nums[0]
+        # l = len(nums)
 
-        if l == 1 or k == 0:
-            return
+        # if l == 1 or k == 0:
+        #     return
 
-        while reps < l:
-            reps += 1
-            idx = (i + k) % l
-            c = nums[idx]
-            nums[idx] = temp
-            temp = c
-            i = idx
-            if k * reps % l == 0:
-                i += 1
-                temp = nums[i % l]
+        # while reps < l:
+        #     reps += 1
+        #     idx = (i + k) % l
+        #     c = nums[idx]
+        #     nums[idx] = temp
+        #     temp = c
+        #     i = idx
+        #     if k * reps % l == 0:
+        #         i += 1
+        #         temp = nums[i % l]
+
+        # OPTIMAL APPROACH
+
+        k %= len(nums)
+
+        l, r = 0, len(nums) - 1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l, r = l + 1, r - 1
+
+        l, r = 0, k - 1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l, r = l + 1, r - 1
+        
+        l, r = k, len(nums) - 1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l, r = l + 1, r - 1
+
+
+        
+
+
